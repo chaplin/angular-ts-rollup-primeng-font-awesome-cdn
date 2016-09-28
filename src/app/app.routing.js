17e8ef7a51774043daf8c9e94e634a0c22c6ccf1
ShresthaSam <shresthasamr@gmail.com>
2016-09-28 13:46:03 -0400

    removed TS generated files

--- src/app/app.routing.js
+++
@@ -1,9 +0,0 @@
-import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-var appRoutes = [
-    { path: 'header', component: HeaderComponent },
-    { path: '', component: HeaderComponent }
-];
-export var appRoutingProviders = [];
-export var appRouting = RouterModule.forRoot(appRoutes);
-//# sourceMappingURL=app.routing.js.map
